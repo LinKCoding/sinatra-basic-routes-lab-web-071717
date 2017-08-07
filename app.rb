e37da192f8b831e7f8814438c21319fafa1add01
linkcoding <kenny.lin.91@gmail.com>
2017-08-07 17:53:36 -0400

    created routes

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Slim Shady"
+  end
+
+  get '/hometown' do
+    "My hometown is Detriot, Michigan"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is '8-mile'"
+  end
+  
+  get '/' do
+    "Hello, World!"
+  end
+
 end
